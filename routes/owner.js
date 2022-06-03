b128d648bda9c32965913bc04c13337130cc63a8
Maddy Tan <maddytan7@gmail.com>
2022-06-03 19:59:46 +0000

    includes _header partial and initial index page with customer/owner links

--- routes/owner.js
+++ routes/owner.js
@@ -8,6 +8,22 @@
 const express = require('express');
 const router  = express.Router();
 
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+  return router;
+};
+
 // GET request when order is created that triggers an SMS notification
 // POST request for the owner to update the order time via SMS or webpage
   // triggers a notification to customer on when order is ready
