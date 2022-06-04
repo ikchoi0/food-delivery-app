4782ecd24b8a22cc1d06968d5640c3d4c4808e59
Ik Sung Choi <ikchoi0@gmail.com>
2022-06-03 20:56:35 -0400

    add more menu data

--- db/seeds/04_menu.sql
+++ db/seeds/04_menu.sql
@@ -1,3 +1,11 @@
-INSERT INTO menu (name, description, photo_url, price)
+INSERT INTO menus (name, description, photo_url, price)
 VALUES ('Chipotle Lobster', 'Japanese Milk Bread, Folded Eggs, Lobster, Bacon, Corn, Lettuce, Chipootle sauce', 'https://media.blogto.com/articles/20201211-EggClub-9.jpg?w=720&cmd=resize_then_crop&height=480&quality=70',1300),
-('Creamy Corn Salsa', 'Japanese Milk Bread, Folded Eggs, Corn Salsa, Creamy Chili Sauce', 'https://lh3.googleusercontent.com/j2rMXWFVo0sQgm2u5OrT0JTu6pP3EMOibTD2ZlUdLcU1zg9p9jHppphOadvMPOp2cnSc86fG31KiTrh02LW5PbTS2fQ', 1100);
+('Creamy Corn Salsa', 'Japanese Milk Bread, Folded Eggs, Corn Salsa, Creamy Chili Sauce', 'https://lh3.googleusercontent.com/j2rMXWFVo0sQgm2u5OrT0JTu6pP3EMOibTD2ZlUdLcU1zg9p9jHppphOadvMPOp2cnSc86fG31KiTrh02LW5PbTS2fQ', 1100),
+('Karubi Don	Angus', 'boneless beef short rib, onion. Grilled and broiled meat on top of Japanese short grain rice (fresh meat fresh grill).',	'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/4e1121d6-b571-4958-b74b-759f9e7b9d30-retina-large-jpeg',	2459),
+('Karaage', 'Japanese popcorn chicken', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/857f6c57-235f-4045-bc6f-723fb945b557-retina-large-jpeg', 875),
+('Gyu Don', 'Beef slice, onion, beni-shoga.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/6288f7ef-8f04-4c9f-a828-1946d6510426-retina-large-jpeg', 1789),
+('Katsu Don', 'Panko fried pork or chicken, egg mixture, onion.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/a78f6277-fe43-4635-8b9c-e34a5f8adb13-retina-large-jpeg', 1789),
+('Ton-Toro Don', 'Pork jowl meat, onion. Grilled and broiled meat on top of Japanese short grain rice (fresh meat fresh grill).', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/d6af64b4-c40b-48ac-a3de-66ec2dafe1bb-retina-large-jpeg', 1829),
+('Sake - Oshizushi', 'Blowtorched Atlantic salmon, furikake, tobiko, green onion, sushi rice.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/aa091e62-c159-4239-b566-4035983ed95f-retina-large-jpeg', 1789),
+('Miso Salmon Don', 'Miso cured Atlantic salmon.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/9eb2d1b7-08b8-4349-adfe-0973bd4d347b-retina-large-jpeg', 1889),
+('yakiniku don', 'grill marinated boneless Angus beef short rib, onion.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/630c6b3f-ebc8-4d99-9118-fa87e9f5f83b-retina-large-jpeg', 2259)
