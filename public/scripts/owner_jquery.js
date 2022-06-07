f7178f539d553203b21aee4d3c13a9c0525832af
Ik Sung Choi <ikchoi0@gmail.com>
2022-06-07 16:07:48 -0400

    fix complete, confirm handling on owner route

--- public/scripts/owner_jquery.js
+++ public/scripts/owner_jquery.js
@@ -1,53 +1,33 @@
 $(() => {
-
-$('.decline').on('submit', function(e) {
-  e.preventDefault();
-  const data = $(this).serialize();
-  $.post("/api/owner/decline", data)
+  $(".decline").on("submit", function (e) {
+    e.preventDefault();
+    const data = $(this).serialize();
+    $.post("/api/owner/decline", data);
     $(this).parent().parent().parent().remove();
-});
+  });
 
-$('.confirm').on('submit', function(e) {
-  e.preventDefault();
-  const formData= $(this).serialize();
-  $.post("/api/owner/confirm", formData, (result, error) => {
-    $(`.order_form-${result.data.id}`).hide();
-
-    renderTimer(result.confirm_time, result.data.id);
-
-    $('.completed').on('submit', function(e) {
-      e.preventDefault();
-      const completedId = result.data.id;
-      $.post("/api/owner/confirm/completed", {completedId})
-      $(`.order-${completedId}`).remove();
+  $(".confirm_order_form").on("submit", function (e) {
+    e.preventDefault();
+    const orderId = $(this).serialize();
+    console.log(orderId);
+    $.post("/api/owner/order/confirm", orderId)
+    .then(response => {
+      window.location.replace('http://localhost:8080/api/owner');
     })
-  })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+
+  $(".complete_order_form").on("submit", function (e) {
+    e.preventDefault();
+    const orderId = $(this).serialize();
+    $.post("/api/owner/order/complete", orderId)
+    .then(response => {
+      window.location.replace('http://localhost:8080/api/owner');
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
 });
-
-});
-
-const renderTimer = (time, orderId) => {
-  const $timer = `
-  <div>
-    <h2 class="countdown">${time}</h2>
-    <label>mins</label>
-  </div>
-  <form class="completed" method="POST">
-  <div>
-    <button type="submit">completed</button>
-  </div>
-  </form>
-  `
-  $(`.counter_form-${orderId}`).append($timer);
-
-    var doUpdate = function() {
-      $('.countdown').each(function() {
-        var count = parseInt($(this).html());
-        if (count !== 0) {
-          $(this).html(count - 1);
-        }
-      });
-    };
-
-    setInterval(doUpdate, 60000);
-}
--- routes/owner.js
+++ routes/owner.js
@@ -5,84 +5,116 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { authenticateUser, authenticateOwner } = require("../lib/auth-helper");
 
 module.exports = (db) => {
-  router.get("/menu/create", authenticateUser, authenticateOwner, (req, res) => {
-    res.render("add_menu", {"user": req.session});
-  });
+  router.get(
+    "/menu/create",
+    authenticateUser,
+    authenticateOwner,
+    (req, res) => {
+      res.render("add_menu", { user: req.session });
+    }
+  );
   router.post("/menu", (req, res) => {
     const {
       create_menu_name,
       create_menu_photo_url,
       create_menu_description,
-      create_menu_price
+      create_menu_price,
     } = req.body;
     db.query(
       `
         INSERT INTO menus (name, photo_url, description, price)
         VALUES ($1, $2, $3, $4);
-      `, [create_menu_name, create_menu_photo_url, create_menu_description, Number(create_menu_price)]
-    ).then(() => {
-      res.redirect("/api/owner");
-    }).catch(error => {
-      console.log(error);
-    });
+      `,
+      [
+        create_menu_name,
+        create_menu_photo_url,
+        create_menu_description,
+        Number(create_menu_price),
+      ]
+    )
+      .then(() => {
+        res.redirect("/api/owner");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   });
 
   router.get("/", authenticateUser, authenticateOwner, (req, res) => {
     const orders = [];
 
-    db.query(`
-      SELECT orders.id AS order_id, ARRAY_AGG(menus.name) AS menu_name, COUNT(menus.*) AS total_items, orders.order_placed_at AS placed_time
-      FROM orders
-      JOIN items_ordered ON orders.id = items_ordered.order_id
-      JOIN menus ON menus.id = items_ordered.menu_id
-      JOIN customers ON customers.id = orders.customer_id
-      GROUP BY orders.id;
-         `
-    ).then(data => {
+    db.query(
+      `
+        SELECT orders.id AS order_id, ARRAY_AGG(menus.name) AS menu_name, COUNT(menus.*) AS total_items, orders.order_placed_at AS placed_time, order_started_at, order_completed_at
+        FROM orders
+        JOIN items_ordered ON orders.id = items_ordered.order_id
+        JOIN menus ON menus.id = items_ordered.menu_id
+        JOIN customers ON customers.id = orders.customer_id
+        GROUP BY orders.id;
+      `
+    )
+    .then((data) => {
       orders.push(...data.rows);
-      res.render('owner', { orders: orders, user: req.session });
-    }).catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
+      res.render("owner", { orders: orders, user: req.session });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: err.message });
     });
   });
 
   router.post("/decline", authenticateUser, authenticateOwner, (req, res) => {
     const { orderId } = req.body;
-    db.query(`DELETE FROM orders WHERE id = $1;`, [orderId]).then(data => {
+    db.query(`DELETE FROM orders WHERE id = $1;`, [orderId]).then((data) => {
       res.send(data.rows[0]);
-    })
-  })
+    });
+  });
 
-  router.post("/confirm",  authenticateUser, authenticateOwner, (req, res) => {
-    const { orderId, confirm_time } = req.body;
-    db.query(`UPDATE orders
-    SET order_started_at = NOW()
-    WHERE id = $1 RETURNING *;`, [orderId]
-    ).then(data => {
-      res.send({data: data.rows[0], confirm_time});
-    })
+  router.post(
+    "/order/confirm",
+    authenticateUser,
+    authenticateOwner,
+    (req, res) => {
+      const { orderId } = req.body;
+      db.query(
+        `
+          UPDATE orders
+          SET order_started_at = NOW()
+          WHERE id = $1
+          RETURNING *;
+        `, [orderId]
+      )
+      .then((data) => {
+        res.send({ data: data.rows[0] });
+      });
+    }
+  );
 
-    router.post("/confirm/completed", authenticateUser, authenticateOwner, (req, res) => {
-      const { completedId } = req.body;
-      db.query(`UPDATE orders
-       SET order_completed_at = NOW()
-       WHERE id = $1 RETURNING *;
-       `, [completedId]).then(data => {
+  router.post(
+    "/order/complete",
+    authenticateUser,
+    authenticateOwner,
+    (req, res) => {
+      const { orderId } = req.body;
+      db.query(
+        `
+          UPDATE orders
+          SET order_completed_at = NOW()
+          WHERE id = $1 RETURNING *;
+        `, [orderId]
+      )
+      .then((data) => {
         res.send(data.rows);
-      })
-    })
-
-  })
+      });
+    }
+  );
   return router;
 };
 
 // GET request when order is created that triggers an SMS notification
 // POST request for the owner to update the order time via SMS or webpage
-  // triggers a notification to customer on when order is ready
+// triggers a notification to customer on when order is ready
