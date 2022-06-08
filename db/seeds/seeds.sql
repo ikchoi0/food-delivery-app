d6f710f3ea908356cee8b38cf8e35e276a00a881
Ik Sung Choi <ikchoi0@gmail.com>
2022-06-08 16:56:32 -0400

    add more data in tables

--- db/seeds/seeds.sql
+++ db/seeds/seeds.sql
@@ -1,30 +1,54 @@
 INSERT INTO customers (name, email, phone_number, password)
 VALUES ('owner', 'owner@owner.com', '111-222-3333', '$2b$10$Rk1698ZklIz.udiVSN8xROyNixWy5syDuwQdXQsiyihptXAVtClwa'),
-('Emma', 'emem@gmail.com', '437-217-2607', 'udiVSN8xROyNixWy5syDuwQdXQsiyihptXAVtClwa'),
 ('customer', 'customer@customer.com', '437-217-1111', '$2b$10$Rk1698ZklIz.udiVSN8xROyNixWy5syDuwQdXQsiyihptXAVtClwa');
 
-
-INSERT INTO orders (customer_id, order_placed_at, order_started_at)
-VALUES (1, '2022-05-21 12:32:45', '2022-05-21 12:33:11'),(2, '2022-05-21 12:32:45', '2022-05-21 12:33:11');
-
 INSERT INTO menus (name, description, photo_url, price)
-VALUES ('Chipotle Lobster', 'Japanese Milk Bread, Folded Eggs, Lobster, Bacon, Corn, Lettuce, Chipootle sauce', 'https://media.blogto.com/articles/20201211-EggClub-9.jpg?w=720&cmd=resize_then_crop&height=480&quality=70',1300),
-('Creamy Corn Salsa', 'Japanese Milk Bread, Folded Eggs, Corn Salsa, Creamy Chili Sauce', 'https://lh3.googleusercontent.com/j2rMXWFVo0sQgm2u5OrT0JTu6pP3EMOibTD2ZlUdLcU1zg9p9jHppphOadvMPOp2cnSc86fG31KiTrh02LW5PbTS2fQ', 1100),
+VALUES ('Yakiniku Fire Don', 'grill marinated boneless Angus beef short rib, onion, chili sauce.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/1495c312-237c-43d8-8d74-1ead8de32611-retina-large-jpeg',2389),
 ('Karubi Don	Angus', 'boneless beef short rib, onion. Grilled and broiled meat on top of Japanese short grain rice (fresh meat fresh grill).',	'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/4e1121d6-b571-4958-b74b-759f9e7b9d30-retina-large-jpeg',	2459),
-('Karaage', 'Japanese popcorn chicken', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/857f6c57-235f-4045-bc6f-723fb945b557-retina-large-jpeg', 875),
 ('Gyu Don', 'Beef slice, onion, beni-shoga.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/6288f7ef-8f04-4c9f-a828-1946d6510426-retina-large-jpeg', 1789),
 ('Katsu Don', 'Panko fried pork or chicken, egg mixture, onion.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/a78f6277-fe43-4635-8b9c-e34a5f8adb13-retina-large-jpeg', 1789),
 ('Ton-Toro Don', 'Pork jowl meat, onion. Grilled and broiled meat on top of Japanese short grain rice (fresh meat fresh grill).', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/d6af64b4-c40b-48ac-a3de-66ec2dafe1bb-retina-large-jpeg', 1829),
-('Sake - Oshizushi', 'Blowtorched Atlantic salmon, furikake, tobiko, green onion, sushi rice.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/aa091e62-c159-4239-b566-4035983ed95f-retina-large-jpeg', 1789),
 ('Miso Salmon Don', 'Miso cured Atlantic salmon.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/9eb2d1b7-08b8-4349-adfe-0973bd4d347b-retina-large-jpeg', 1889),
-('yakiniku don', 'grill marinated boneless Angus beef short rib, onion.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/630c6b3f-ebc8-4d99-9118-fa87e9f5f83b-retina-large-jpeg', 2259);
+('yakiniku don', 'grill marinated boneless Angus beef short rib, onion.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/630c6b3f-ebc8-4d99-9118-fa87e9f5f83b-retina-large-jpeg', 2259),
+('Aburi Salmon Don', 'lightly seared fresh salmon with sweet mayo, tobiko(fish roe)', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/630c6b3f-ebc8-4d99-9118-fa87e9f5f83b-retina-large-jpeg', 2219),
+('Unagi Don', 'Hot. Spicy grilled pork belly, onion. Grilled and broiled meat on top of Japanese short grain rice (fresh meat fresh grill).', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/22f24512-7673-45e2-9520-ca023a25c140-retina-large-jpeg', 2459),
+('Bacon Steakie Fire Don', 'lightly seared fresh salmon with sweet mayo, tobiko(fish roe)', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/56ddf55e-5a3a-4cdb-adef-b7001e4b97b5-retina-large-jpeg', 1889),
+('Bacon Steakie Don', 'Grilled pork belly, onion. Grilled and broiled meat on top of Japanese short grain rice (fresh meat fresh grill).', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/04c68804-dced-41c6-8c3b-82d43456f26a-retina-large-jpeg', 1889),('Gyu-Tan don
+', 'grill marinated beef tongue, onion. (crunchy, tender, juicy)', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/8f2d7fd3-f3e5-4873-beff-d7230cf9071c-retina-large-jpeg', 2039),
+('Gyu Curry', 'angus beef, carrots, pineapple, Japanese curry sauce.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/dc5fe50d-48d3-4a90-ab22-29cebdf6a5f8-retina-large-jpeg', 2089),
+('Sake - Oshizushi', 'Blowtorched Atlantic salmon, furikake, tobiko, green onion, sushi rice.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/aa091e62-c159-4239-b566-4035983ed95f-retina-large-jpeg', 1789),
+('Karaage', 'Japanese popcorn chicken', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/857f6c57-235f-4045-bc6f-723fb945b557-retina-large-jpeg', 875),
+('ebi-furai', 'panko fried shrimp.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/de9170bb-a047-45cd-9f25-34689dee25d1-retina-large-jpeg', 749),
+('Pork Cutlet', 'panko fried pork.(antibiotic free)', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/63b35323-a04b-40ee-8c8d-96412ecf23d6-retina-large-jpeg', 889),
+('Sweet Potato Stick', 'recommend order this item dine-in only, because the deep fried stick might become soggy or messy when it arrives to you.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/47e9cb2b-5879-4803-8c09-215a25e704b9-retina-large-jpeg', 659),
+('cabbage salad', 'with sesame dressing', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photosV2/3b8a61b1-30b3-4712-ab31-173dec6e455e-retina-large.JPG', 355),
+('hot spring egg', 'poached egg and serve it cold.', 'https://img.cdn4dd.com/p/fit=cover,width=600,format=auto,quality=50/media/photos/cada94d2-b754-4b24-ac1f-42bb8e945faa-retina-large-jpeg', 249);
+
+INSERT INTO orders (customer_id, order_placed_at, order_started_at, order_completed_at)
+VALUES
+(2, '2022-05-28 17:12:31', '2022-05-28 17:14:31', '2022-05-28 17:50:31'),
+(2, '2022-05-29 17:12:31', '2022-05-29 17:13:31', '2022-05-29 18:12:31'),
+(2, '2022-05-30 14:23:40', '2022-05-30 14:30:40', '2022-05-30 15:23:40'),
+(2, '2022-06-01 18:34:21', '2022-06-01 18:35:21', '2022-06-01 19:30:21'),
+(2, '2022-06-03 20:43:22', '2022-06-03 20:49:22', '2022-06-03 21:20:22'),
+(2, '2022-06-05 20:43:22', '2022-06-05 20:50:22', '2022-06-05 21:50:22');
 
 INSERT INTO items_ordered (order_id, menu_id)
 VALUES
-(2, 1),
-(2, 6),
-(2, 5),
-(2, 8);
-
-
+(1, 1),
+(2, 2),
+(2, 2),
+(2, 2),
+(2, 2),
+(3, 9),
+(3, 10),
+(3, 8),
+(4, 2),
+(4, 2),
+(4, 2),
+(5, 8),
+(5, 2),
+(5, 2),
+(5, 2),
+(5, 8);
 
